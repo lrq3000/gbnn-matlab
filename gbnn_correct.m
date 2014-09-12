60bd555308f93da4a0ffb4a700b4744d8bcde17c
Stephen L <lrq3000@gmail.com>
2014-09-12 17:38:46 +0200

    edit: tags: maybe better perfs
    Signed-off-by: Stephen L. <lrq3000@gmail.com>

--- gbnn_correct.m
+++ gbnn_correct.m
@@ -939,31 +939,32 @@ for diter=1:diterations
                     decoded_edges = net(decoded_fanals, decoded_fanals) ; % fetch tags of only the edges of the recovered clique (thus edges connected between fanals in the recovered clique but not those that are not part of the clique, ie connected to another fanal outside the clique, won't be included)
                     if nnz(decoded_edges) == 0; continue; end; % if this message is empty then just quit
 
+                    decoded_edges_tags = decoded_edges;
                     % Filter useless fanals (fanals that do not possess as many edges as the maximum - meaning they're not part of the clique). Note: This is a pre-processing enhancement step, but it's not necessary if you use fastmode(nonzeros(decoded_edges)) (the nonzeros will take care of the false 0 tag) BUT it greatly enhances the performances when using iterations > 1.
-%                    if concurrent_cliques == 1 && ~concurrent_disequilibrium % use GWTA to filter if we have only one clique and no disequilibrium, because if we have multiple concurrent cliques this won't work because the cliques may have different number of edges and shared fanals may have a lot of edges, but only this shared fanal will be kept and the others correct fanals will be filtered out because they have less edges than the shared fanal. Also avoid if concurrent_disequilibrium is enabled, for similar reasons AND because we don't want to filter out possibly correct fanals, which this does (check with matching measure, this trick here lowers down the matching).
+                    if concurrent_cliques == 1 && ~concurrent_disequilibrium % use GWTA to filter if we have only one clique and no disequilibrium, because if we have multiple concurrent cliques this won't work because the cliques may have different number of edges and shared fanals may have a lot of edges, but only this shared fanal will be kept and the others correct fanals will be filtered out because they have less edges than the shared fanal. Also avoid if concurrent_disequilibrium is enabled, for similar reasons AND because we don't want to filter out possibly correct fanals, which this does (check with matching measure, this trick here lowers down the matching).
                         % Filter using GWTA (keep only the fanals with max score)
-%                        fanal_scores = sum(sign(decoded_edges));
-%                        winning_score = max(fanal_scores);
-%                        gwta_mask = (fanal_scores == winning_score);
-%                        decoded_edges = decoded_edges(gwta_mask, :) ;
-%                    else % else i we have concurrent cliques and/or disequilibrium, use GWSTA to filter (because GWTA won't work in concurrent case).
+                        fanal_scores = sum(sign(decoded_edges));
+                        winning_score = max(fanal_scores);
+                        gwta_mask = (fanal_scores == winning_score);
+                        decoded_edges_tags = decoded_edges(gwta_mask, :) ;
+                    else % else i we have concurrent cliques and/or disequilibrium, use GWSTA to filter (because GWTA won't work in concurrent case).
                     % NOTE: this is even less necessary than in the non concurrent cliques case, here it only provides a small performance boost (but significative), so it's up to you to see if the additional CPU time needed to do the GWSTA filtering is worth it, but keep in mind that filtering also speeds up the tags filtering below, because we remove fanals whose edges won't have to be tag checked!
-%                        fanal_scores = sum(sign(decoded_edges));
-%                        if numel(fanal_scores) > (c * concurrent_cliques)
+                        fanal_scores = sum(sign(decoded_edges));
+                        if numel(fanal_scores) > (c * concurrent_cliques)
                             % Filter using GWSTA (reject fanals below the kth max score)
-%                            max_scores = sort(fanal_scores,'descend'); % sort scores
-%                            kmax_score = max_scores(k); % get the kth max score
-%                            kmax_score(kmax_score == 0) = realmin(); % No false winner trick: filter out kth winners that have value 0 by replacing the threshold with the minimum real value (greater than 0)
-%                            gwsta_mask = fanal_scores >= kmax_score; % filter out all fanals that get a score below the kth winner
-%                            decoded_edges = decoded_edges(gwsta_mask, :); % update our network
+                            max_scores = sort(fanal_scores,'descend'); % sort scores
+                            kmax_score = max_scores(k); % get the kth max score
+                            kmax_score(kmax_score == 0) = realmin(); % No false winner trick: filter out kth winners that have value 0 by replacing the threshold with the minimum real value (greater than 0)
+                            gwsta_mask = fanal_scores >= kmax_score; % filter out all fanals that get a score below the kth winner
+                            decoded_edges_tags = decoded_edges(gwsta_mask, :); % update our network
                             %decoded_edges = decoded_edges(:, gwsta_mask); % update our network
                             %decoded_fanals = decoded_fanals(gwsta_mask);
                             
                             % Alternative methods with lesser performances
                             %decoded_edges = decoded_edges((fanal_scores ~= min(nonzeros(fanal_scores))), :);
                             %decoded_edges = decoded_edges(ismember(fanal_scores, aux.fastmode(nonzeros(fanal_scores))), :);
-%                        end
-%                    end
+                        end
+                    end
 
 % IDEE: utiliser le filtrage pour trouver tag majeur, mais ensuite reprendre le decoded_edges original pour le truc final
 % TODO: fix quand gwsta_mask est bien sur les colonnes et non pas sur les lignes (ca buggue alors que c'est bon normalement! donc pb doit etre dans les indexs à la fin)
@@ -971,12 +972,12 @@ for diter=1:diterations
 
                     % Filter edges having a tag different than the major tag, and then filter out fanals that gets disconnected from the clique (all their incoming edges were filtered because they were of a different tag than the major tag)
                     if concurrent_cliques == 1
-                        major_tag = aux.fastmode(nonzeros(decoded_edges)) ; % get the major tag (the one which globally appears the most often in this clique). NOTE: nonzeros somewhat slows down the processing BUT it's necessary to ensure that 0 is not chosen as the major tag (since it represents the absence of edge!) - this problem often happens when using a sparse network (Chi > c).
+                        major_tag = aux.fastmode(nonzeros(decoded_edges_tags)) ; % get the major tag (the one which globally appears the most often in this clique). NOTE: nonzeros somewhat slows down the processing BUT it's necessary to ensure that 0 is not chosen as the major tag (since it represents the absence of edge!) - this problem often happens when using a sparse network (Chi > c).
                         %major_tag = aux.fastmode(cellfun(@min, aux.nnzcolmode(decoded_edges))); % IDEE: faire un mode par colonne, et ensuite faire le mode de tous. Ca permettra de supprimer le biais si on a écrasé des liens d'un fanal (ce qui fait qu'on va le filtrer au GWTA puisque basé sur le nombre d'arêtes entrantes...), mais il faut quand meme qu'on filtre car sinon on risque de faire major wrong! Comme ça, meme si plusieurs fanaux n'ont que d'autres tags, il suffit que pour quelques fanaux ce soit encore le tag majoritaire et que les fanaux bien écrasés aient au moins une fois ce tag pour que le message soit bien récupéré.
                         % IDEE ML pour tags net: on regarde tous les tags uniques et on garde le premier qui forme une clique.
                         decoded_edges(decoded_edges ~= min(major_tag)) = 0 ; % shutdown edges who haven't got the maximum tag. NOTE: in case of ambiguity (two or more major tags), we keep the minimum (oldest) one.
                     else
-                        major_tag = aux.kfastmode(nonzeros(decoded_edges), concurrent_cliques);
+                        major_tag = aux.kfastmode(nonzeros(decoded_edges_tags), concurrent_cliques);
                         decoded_edges(~ismember(decoded_edges, major_tag)) = 0; % shutdown edges who haven't got the maximum tag. NOTE: in case of ambiguity (two or more major tags), we keep them all. This seems to enhance performances a bit compared to select the minimum one or a random one.
                     end
                     decoded_fanals = decoded_fanals(sum(decoded_edges) ~= 0) ; % kick out fanals which have no incoming edges after having deleted edges without major tag (ie: nodes that become isolated because their edges had different tags than the major tag will just be removed, because if these nodes become isolated it's because they obviously are part of another message, else they would have at least one edge with the correct tag).
